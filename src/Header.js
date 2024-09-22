cdf80b5315ca6f89dace3068df61837195f2f4d5
Mohit Verma <mohitver01@gmail.com>
2024-09-22 16:55:35 -0400

    added prop drilling

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
-const Header = () => {
+const Header = ( { title } ) => {
 
     return (
         <header>
-            <h1>Groceries List</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
+
+// Default props - let us values for props expedcted in component. If not provided, default vaue takes over instead of error
+// Good way to spec component and not receinvg data from API or local storage
+Header.defaultProps = {
+    title : "Default List"
+}
+
+
 
 export default Header;
